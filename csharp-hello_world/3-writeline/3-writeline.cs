42277fb182238533922b7541051668e77dc6cd62
Rbrogniez <5013@holbertonschool.com>
2023-07-24 13:55:13 +0200

    try new code for writeline.cs

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Programming is like building a multilingual puzzle");
+﻿using System;
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Programming is like building a multilingual puzzle");
+	}
+}
