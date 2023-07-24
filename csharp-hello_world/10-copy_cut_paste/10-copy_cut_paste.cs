f3c765fd7a4b1dc07ae0e1cd35c0592986077108
Rbrogniez <5013@holbertonschool.com>
2023-07-24 18:17:23 +0200

    creat 3 strings

---
+++ csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+        static void Main(string[] args)
+        {
+		string word = "Holberton";
+        string word_first_3 = "Hol";
+        string word_last_2 = "on";
+        string middle_word = "olberto";
+		Console.WriteLine("First 3 letters: {0}", word_first_3);
+		Console.WriteLine("Last 2 letters: {0}", word_last_2);
+		Console.WriteLine("Middle word: {0}", middle_word);
+        }
+}
