3253696497e76c62b05296cd8da49d1e046cb555
Rbrogniez <5013@holbertonschool.com>
2023-07-24 13:57:22 +0200

    addind \ in writeline

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -3,6 +3,6 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		Console.WriteLine("Programming is like building a multilingual puzzle");
+		Console.WriteLine("\"Programming is like building a multilingual puzzle");
 	}
 }
