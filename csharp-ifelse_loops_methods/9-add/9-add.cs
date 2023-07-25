7dc96f70c8d073f8a7c39abf3534adcfdf855ad0
Rbrogniez <5013@holbertonschool.com>
2023-07-25 14:45:40 +0200

    delete all code

--- csharp-ifelse_loops_methods/9-add/9-add.cs
+++ csharp-ifelse_loops_methods/9-add/9-add.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace _9_add
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿
