320eaf92a662d932ed9de805d47a6ff0dbff5f36
Rbrogniez <5013@holbertonschool.com>
2023-07-25 15:07:53 +0200

    prints the alphabet, in lowercase without e and q letter

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (char letter = 'a'; letter <= 'z'; letter++)
+        {
+            if (letter == 'e' || letter == 'q')
+            {
+                Console.Write("");
+            }
+
+            else
+            {
+                Console.Write(letter + "");
+            }
+        }
+    }
+}
