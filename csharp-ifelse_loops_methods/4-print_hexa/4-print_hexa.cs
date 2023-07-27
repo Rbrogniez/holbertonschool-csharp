df6ebbceae7b3e445d065b5f00c0111de5e1d92e
Rbrogniez <5013@holbertonschool.com>
2023-07-27 03:51:19 +0200

    program to convert INT to HEXA with loop

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 98; i++)
+
+                {
+                    string hex = i.ToString("X");
+                    Console.Write(i + " = " + "0x"+hex + "\n");
+                }
+
+        }
+    }
+}
