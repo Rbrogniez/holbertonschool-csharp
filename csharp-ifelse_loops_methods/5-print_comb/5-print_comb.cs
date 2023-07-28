cb82573a360309f43234b5c7423dfdf15a2a23a6
Rbrogniez <5013@holbertonschool.com>
2023-07-28 10:11:23 +0200

    print number 1 to 99

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i<99; i++)
+                {
+                    Console.Write(i.ToString("D2") + ", ");
+                }
+            Console.Write(i.ToString("D2") + "\n");
+        }
+
+    }
+}
