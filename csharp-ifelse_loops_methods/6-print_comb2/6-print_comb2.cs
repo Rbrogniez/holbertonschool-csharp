9b6e7b2bc9d87ece802099994da358cdeac8ff6b
Rbrogniez <5013@holbertonschool.com>
2023-07-28 10:46:43 +0200

    Write a program that prints all possible different combinations of two digits

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int j = 0;
+            for (i = 0; i<=7 ; i++)
+            {
+                for (j = i ; j<=9 ; j++)
+                {
+                    string number = i.ToString() + j.ToString();
+                    if (i != j)
+                        Console.Write(number + ", ");
+                }
+            }
+            Console.Write((i).ToString() + (j-1).ToString() + "\n");
+        }
+    }
+}
