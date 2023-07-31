5044eb78926d30f43d2fbcacd827e9a5dbd57984
Rbrogniez <5013@holbertonschool.com>
2023-07-31 18:20:23 +0200

    deleting the last space and creat if/else because i don't won't to have space in the last int of my array but lign return

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -16,10 +16,17 @@ using System.Collections.Generic;
         System.Array.Reverse(array);
         for (int i = 0; i < array.Length; i++)
         {
-            Console.Write(array[i]+"  ");
+            if (i == array.Length - 1)
+            {
+                Console.Write("\n");
+            }
+            else
+            {
+                Console.Write(array[i]+" ");
+            }
         }
         }
-        Console.Write("\n");
+
 
         }
     }
