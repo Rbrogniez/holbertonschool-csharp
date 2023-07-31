548fb144144d306361cebb8b44cbc1ef50f1e2a8
Rbrogniez <5013@holbertonschool.com>
2023-07-31 17:03:38 +0200

    Write a method that retrieves an element from an array.

---
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+
+        if (index < 0 || index >= array.Length)
+        {
+            Console.Write("Index out of range"+"\n");
+            return -1;
+        }
+        else
+        {
+            return array[index];
+        }
+
+    }
+}
