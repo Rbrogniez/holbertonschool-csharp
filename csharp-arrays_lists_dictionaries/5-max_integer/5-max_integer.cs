8f1fca3217907a2ddf21544e0403f82d368b354a
Rbrogniez <5013@holbertonschool.com>
2023-08-01 12:33:00 +0200

    Write a method that finds the biggest integer of a list.

---
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+     if(myList.Count == 0)
+     {
+        Console.Write("List is empty ");
+        return -1;
+     }
+     else
+     {
+     int MaxInteger = myList[0];
+     for (int i = 1; i < myList.Count; i++)
+     {
+        if (myList[i] > MaxInteger)
+        {
+            MaxInteger = myList[i];
+        }
+     }
+     return MaxInteger;
+     }
+    }
+}
