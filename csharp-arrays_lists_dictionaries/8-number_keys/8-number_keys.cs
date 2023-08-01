32c03b5dce8c3a4089f89f07d34950b1a6bd64fb
Rbrogniez <5013@holbertonschool.com>
2023-08-01 17:16:46 +0200

    Write a method that returns the number of keys in a dictionary.

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int NumberKeys = 0;
+        foreach (var keys in myDict)
+        {
+            NumberKeys++;
+        }
+        return NumberKeys;
+    }
+}
