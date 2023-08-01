7d1b9e9c2b693474e92c4f29fb43ff132e7f4955
Rbrogniez <5013@holbertonschool.com>
2023-08-01 11:18:38 +0200

    Write a method that creates and prints an array of integers of a given size.

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int> mylist;
+        
+        if (size < 0)
+        {
+            Console.Write("Size cannot be negative");
+            return null;
+        }
+
+        else
+        {
+        mylist = new List<int>(size);
+        for (int i = 0; i < size; i++)
+        {
+            mylist.Add(i);
+        }
+
+        for (int i = 0; i < mylist.Count; i++)
+        {
+            Console.Write(mylist[i]+" ");
+        }
+        }
+        Console.WriteLine();
+        return mylist;
+    }
+}
