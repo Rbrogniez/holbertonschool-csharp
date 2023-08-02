7bec6b38f26a38f2026d6195c3c34458477a03b1
Rbrogniez <5013@holbertonschool.com>
2023-08-02 17:49:31 +0200

    creat matrice 5x5

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Array
+{
+    static void Main(string[] args)
+    {
+        int [,] Matrice = new int [5,5];
+        for (int i=0; i<Matrice.GetLength(0); i++)
+         {
+            for (int j=0; j<Matrice.GetLength(1); j++)
+                {
+                    Console.Write($"{Matrice[i,j]} ");
+                }
+                Console.Write("\n");
+         }
+    }
+}
+
+
