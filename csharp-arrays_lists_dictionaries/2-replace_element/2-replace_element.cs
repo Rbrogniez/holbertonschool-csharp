a3452c443884662997f681cfa559822680e67088
Rbrogniez <5013@holbertonschool.com>
2023-08-02 11:51:51 +0200

    Write a method that replaces an element of an array at a given index.

---
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        if (index < 0 || index >= array.Length)
+        {
+            Console.Write("Index out of range");
+            return array;
+        }
+
+        else
+        {
+            array[index] = n;
+            return array;
+        }
+    }
+}
+
+
