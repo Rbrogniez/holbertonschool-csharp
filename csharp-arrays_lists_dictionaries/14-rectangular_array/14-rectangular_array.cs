09e03e645f7814f34d951413f5aae490eb9eea15
Rbrogniez <5013@holbertonschool.com>
2023-08-03 14:26:22 +0200

    adding ; at the end of Ln9

--- csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -6,7 +6,7 @@ public static class Array
     static void Main(string[] args)
     {
         int [,] Matrice = new int [5,5];
-        Matrice[2, 2] = 1
+        Matrice[2, 2] = 1;
         for (int i=0; i<Matrice.GetLength(0); i++)
          {
             for (int j=0; j<Matrice.GetLength(1); j++)
