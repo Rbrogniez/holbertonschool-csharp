cd2d2d02bc2c52c419d9bf13687451b2d11de130
Rbrogniez <5013@holbertonschool.com>
2023-08-04 14:12:12 +0200

    Write a method that returns a sorted list of common elements in two lists.

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> newList = new List<int>();
+
+        foreach (var item1 in list1)
+        {
+            foreach (var item2 in list2)
+                {
+                    if (item1 == item2)
+                    {
+                        newList.Add(item1);
+                    }
+                }
+        }
+            return newList;
+    }
+}
