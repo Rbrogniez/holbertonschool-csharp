a519bb3038bdfeb09ccdf4c41a9ad77abf1f40f4
Rbrogniez <5013@holbertonschool.com>
2023-08-04 11:26:34 +0200

    Write a method that adds all unique integers in a list.

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+
+       // List<int> distinctList = myList.Distinct().ToList();
+       // return distinctList.Sum();
+       HashSet<int> uniqueElements = new HashSet<int>(myList);
+       return uniqueElements.Sum();
+    }
+
+}
