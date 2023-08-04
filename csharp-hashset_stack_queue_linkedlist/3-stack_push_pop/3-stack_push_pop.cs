f7536e16570699377c4da638d93c033850715bad
Rbrogniez <5013@holbertonschool.com>
2023-08-04 17:55:19 +0200

    Given a Stack<string>, write a method that does the following:

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+
+        Stack<string> tempStack = new Stack<string>(aStack);
+
+        int NumberItem = 0;
+        for(NumberItem = 0; NumberItem < tempStack.Count; NumberItem++)
+        {
+            tempStack.Pop();
+        }
+        Console.WriteLine("Number of items: "+ NumberItem);
+
+        if(aStack.Count > 0)
+        {
+            string TopStack = aStack.Peek();
+            Console.WriteLine("Top item: "+TopStack);
+        }
+
+        else
+        {
+            Console.WriteLine("Stack is empty");
+        }
+
+        bool containsSearched = aStack.Contains(search);
+        Console.WriteLine($"Stack contains \"{search}\": {containsSearched}");
+
+        if (aStack.Contains(search))
+        {
+            while (aStack.Pop() != search)
+            {
+                continue;
+            }
+        }
+
+
+        aStack.Push(newItem);
+        return aStack;
+    }
+
+}
