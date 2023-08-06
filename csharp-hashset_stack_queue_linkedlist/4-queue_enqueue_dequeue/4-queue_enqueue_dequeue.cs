5a32c8cef28319fb101506c0e240b19198ffd82e
Rbrogniez <5013@holbertonschool.com>
2023-08-06 14:46:58 +0200

    write a method for Queue<string>

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        int NumberItem = aQueue.Count;
+        Console.WriteLine("Number of items: "+ NumberItem);
+
+        if (aQueue.Count > 0)
+        {
+            string TopQueue = aQueue.Peek();
+            Console.WriteLine("First item: " + TopQueue);
+        }
+        else
+        {
+            Console.WriteLine("Queue is empty");
+        }
+
+        aQueue.Enqueue(newItem);
+
+        bool containsSearched = aQueue.Contains(search);
+        Console.WriteLine($"Queue contains \"{search}\": {containsSearched}");
+
+        if (aQueue.Contains(search))
+        {
+            while(aQueue.Dequeue() != search)
+            {
+                continue;
+            }
+        }
+
+        return aQueue;
+    }
+}
