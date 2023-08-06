f60cc31783af1713c47ade542b1caf97f5b33fe3
Rbrogniez <5013@holbertonschool.com>
2023-08-06 15:35:53 +0200

    Write a method that returns the number of elements in a LinkedList.

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int numberItem = 0;
+        foreach (var item in myLList)
+        {
+            numberItem += 1;
+        }
+        return numberItem;
+    }
+}
