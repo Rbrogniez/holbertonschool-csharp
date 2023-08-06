c05e7239cf0296390af4d2de7d788322bdf77523
Rbrogniez <5013@holbertonschool.com>
2023-08-06 14:03:33 +0200

    adding readme

--- csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
-class List
+public class List
 {
     public static int Sum(List<int> myList)
     {
+        // Create a HashSet to store unique integers
+        HashSet<int> uniqueIntegers = new HashSet<int>();
+
+        // Iterate through the list and add unique integers to the HashSet
+        foreach (int num in myList)
+        {
+            if (!uniqueIntegers.Contains(num))
+            {
+                uniqueIntegers.Add(num);
+            }
+        }
+
+        // Sum up the unique integers in the HashSet
+        int sum = 0;
+        foreach (int uniqueNum in uniqueIntegers)
+        {
+            sum += uniqueNum;
+        }
+
+        return sum;
+    }
+}
 
        // List<int> distinctList = myList.Distinct().ToList();
        // return distinctList.Sum();
-       HashSet<int> uniqueElements = new HashSet<int>(myList);
-       return uniqueElements.Sum();
-    }
-
-}
+       // HashSet<int> uniqueElements = new HashSet<int>(myList);
+       // return uniqueElements.Sum();
