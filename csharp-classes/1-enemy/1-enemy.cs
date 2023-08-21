db67795c9425f8ced0e976366b3f8f86ef9fdd19
Rbrogniez <5013@holbertonschool.com>
2023-08-21 16:05:54 +0200

    change XLM comment

--- csharp-classes/1-enemy/1-enemy.cs
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -5,13 +5,15 @@ namespace Enemies
     /// new class Zombie
     public class Zombie
     {
-        public int health; /// Public field to store the health of the Zombie.
+        /// Public field to store the health of the Zombie.
+        public int health;
 
 
         /// Public constructor to initialize the Zombie object with default values.
         public Zombie()
         {
-            health = 0; /// Set value of health
+            /// Set value of health
+            health = 0;
         }
     }
 }
