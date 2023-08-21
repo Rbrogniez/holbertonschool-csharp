f5e24f7ff18d96d9dd6c667d9c663a34de799486
Rbrogniez <5013@holbertonschool.com>
2023-08-21 17:38:20 +0200

    print name and health of zombies created

---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace Enemies
+{
+    /// new class Zombie
+    public class Zombie
+    {
+        /// private field to store the health of the Zombie.
+        private int health;
+        private string name = "(No name)";
+
+        /// private field to store the Name of the Zombie
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        /// Public constructor to initialize the Zombie object with default values.
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// Constructor to initialize the Zombie
+        public Zombie(int value)
+        {
+
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+
+        /// Returns the health of the Zombie
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        /// Print name and health of zombies created
+        public override string ToString()
+        {
+            return $"Zombie Name: {name} / Total Health: {health}";
+        }
+
+    }
+}
