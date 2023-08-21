369ea2ab179d883c43e3a1fc93c629dd066feeec
Rbrogniez <5013@holbertonschool.com>
2023-08-21 16:01:10 +0200

    creat constructor Zombie and creat value health

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// new class Zombie
+    public class Zombie
+    {
+        public int health; /// Public field to store the health of the Zombie.
+
+
+        /// Public constructor to initialize the Zombie object with default values.
+        public Zombie()
+        {
+            health = 0; /// Set value of health
+        }
+    }
+}
