45a0fa64555247f089d48d59a4e5013013e60a7b
Rbrogniez <5013@holbertonschool.com>
2023-08-21 17:04:38 +0200

    task 4

---
+++ csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Enemies
+{
+    /// new class Zombie
+    public class Zombie
+    {
+        /// private field to store the health of the Zombie.
+        private int health;
+        private string name = "(No name)";
+
+        /// creat 
+        public Name()
+        {
+            get { return name;}
+            set { name = value;}
+        }
+        /// Public constructor to initialize the Zombie object with default values.
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// Constructor to initialize the Zombie
+        public Zombie(int value)
+        {
+
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+
+        /// Returns the health of the Zombie
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
