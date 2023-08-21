f064ea8a9d368a12412539bf10c189af701db08c
Rbrogniez <5013@holbertonschool.com>
2023-08-21 16:31:39 +0200

    creat constructor to initialize the zombie

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Enemies
+{
+    /// new class Zombie
+    public class Zombie
+    {
+        /// Public field to store the health of the Zombie.
+        public int health;
+
+
+        /// Public constructor to initialize the Zombie object with default values.
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// Constructor to initialize the Zombie
+        public Zombie(int value)
+        {
+
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+    }
+}
