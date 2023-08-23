a7c4bfef864d52f34a58390fa981ed7090c9bb8d
Rbrogniez <5013@holbertonschool.com>
2023-08-23 10:30:19 +0200

    Write a method that adds 2 numbers

---
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyMath
+{
+    class Operations
+    {
+        public static int Add(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+    }
+}
