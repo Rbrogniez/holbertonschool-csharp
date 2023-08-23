162f75e566f921d72faaa8eddaa8f5cc0e7bb13a
Rbrogniez <5013@holbertonschool.com>
2023-08-23 13:04:44 +0200

    Divide a matrix

---
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,57 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [Test]
+
+        public void Test1()
+        {
+            int[,] matrix = new int[,]
+            {
+                {4,8},
+                {8,16}
+            };
+
+            int[,] ResultMatrix = new int[,]
+            {
+                {2,4},
+                {4,8}
+            };
+
+            Assert.AreEqual(Matrix.Divide(matrix,2),ResultMatrix);
+        }
+
+
+        [Test]
+
+        public void Test2()
+        {
+            int[,] matrix = new int[,]
+            {
+                {4,8},
+                {8,16}
+            };
+
+            Assert.AreEqual(Matrix.Divide(matrix,0),null);
+        }
+
+
+
+        [Test]
+
+        public void Test3()
+        {
+            Assert.AreEqual(Matrix.Divide(new int[,] {{10,20},{20,40}},2),new int[,] {{5,10},{10,20}});
+        }
+
+        [Test]
+
+        public void Test4()
+        {
+            Assert.AreEqual(Matrix.Divide(new int[,] {{10,20},{20,40}},0),null);
+        }
+
+    }
+}
---
+++ csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyMath
+{
+
+    public class Matrix
+    {
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            if(matrix == null)
+            {
+                return null;
+            }
+
+            int Rows = matrix.GetLength(0);
+            int Cols = matrix.GetLength(1);
+
+            int[,] result = new int[Rows, Cols];
+            try
+            {
+                for(int i = 0; i < Rows; i++)
+                {
+                    for(int j = 0; j < Cols; j++)
+                    {
+                        result[i, j] = matrix[i, j] / num;
+                    }
+                }
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("Num cannot be 0");
+                return null;
+            }
+            return result;
+        }
+
+    }
+}
