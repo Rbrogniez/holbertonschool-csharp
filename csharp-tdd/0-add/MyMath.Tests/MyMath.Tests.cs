c955dd2dcb9309e4e71ff384e62234471fa4b6be
Rbrogniez <5013@holbertonschool.com>
2023-08-23 11:04:15 +0200

    creat 3 tests add

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,18 +1,27 @@
 using NUnit.Framework;
 
-namespace Tests
+namespace MyMath.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
+        /// Test add two positive numbers
         public void Test1()
         {
-            Assert.Pass();
+            Assert.AreEqual(Operations.Add(9,4), 13);
         }
+        /// Test add one negative and one negative number
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(Operations.Add(-1,9), 8);
+        }
+        /// Test add two negative numbers
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(Operations.Add(-23,-78), -101);
+        }
+
     }
 }
--- csharp-tdd/0-add/MyMath/MyMath.cs
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -2,12 +2,15 @@
 
 namespace MyMath
 {
-    class Operations
+
+    public class Operations
     {
+        /// Method that add 2 int
         public static int Add(int a, int b)
         {
             int result = a + b;
             return result;
+
         }
     }
 }
