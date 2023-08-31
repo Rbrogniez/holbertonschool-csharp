9884cfa1731ec97e5a91d78dcca30a966bde0e6d
Rbrogniez <5013@holbertonschool.com>
2023-08-31 14:53:52 +0200

    method that returns True if the object is an instance of, or if the object is an instance of a class that inherited from, Array, otherwise return False.

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,22 @@
+﻿﻿using System;
+using System.Collections.Generic;
+
+
+/// Obj class
+class Obj
+{
+
+    /// Returns True if the object is an instance of, or if the object is an instance of a class that inherited from, Array
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if(obj.GetType().IsArray)
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+    }
+}
