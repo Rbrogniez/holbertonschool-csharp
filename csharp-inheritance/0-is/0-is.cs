fad8f54d6fb8dae80788185ae029d8fb28bc6c52
Rbrogniez <5013@holbertonschool.com>
2023-08-31 14:27:06 +0200

    XML

--- csharp-inheritance/0-is/0-is.cs
+++ csharp-inheritance/0-is/0-is.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 
-
+/// Obj class
 class Obj
     {
+        /// returns True if the object is an int
         public static bool IsOfTypeInt(object obj)
         {
             if (obj is int)
