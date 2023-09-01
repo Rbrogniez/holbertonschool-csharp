deadf82116c6558248d7eca2e2dc6666f795d0fc
Rbrogniez <5013@holbertonschool.com>
2023-09-01 13:58:29 +0200

    override int and string

---
+++ csharp-inheritance/7-shape/7-shape.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class Shape
+
+{
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+public class Rectangle : Shape
+{
+    /// Private fields for width and height
+    private int width;
+    private int height;
+
+     /// Public property for width
+    public int Width
+    {
+        get { return width; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            width = value;
+        }
+    }
+
+    /// Public property for height
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            height = value;
+        }
+    }
+
+    // Override the Area() method from the base class
+    public override int Area()
+    {
+        return Width * Height;
+    }
+
+    // Override the ToString() method to provide a custom string representation
+    public override string ToString()
+    {
+        return $"[Rectangle] {Width} / {Height}";
+    }
+
+
+}
