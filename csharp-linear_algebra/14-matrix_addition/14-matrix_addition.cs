ea95c7539084752666646ba6de2fc58ae2d59a67
Rbrogniez <5013@holbertonschool.com>
2023-09-13 16:44:14 +0200

    Comment my code

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -1,27 +1,36 @@
 ﻿using System;
 
+// Définition de la classe MatrixMath
 class MatrixMath
 {
+    // Déclaration de la méthode Add
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
-  {
-        if (matrix1.GetLength(0) != matrix1.GetLength(1) || matrix2.GetLength(0) != matrix2.GetLength(1) || matrix1.GetLength(0) != matrix2.GetLength(0))
+    {
+        // Vérification de la compatibilité des matrices pour l'addition
+        if (matrix1.GetLength(0) != matrix1.GetLength(1) || // Si matrix1 n'est pas carrée
+            matrix2.GetLength(0) != matrix2.GetLength(1) || // Si matrix2 n'est pas carrée
+            matrix1.GetLength(0) != matrix2.GetLength(0))   // Si les dimensions des matrices ne correspondent pas
         {
-            return new double [1,1] { { -1 } };
+            // Retourne une matrice 2x2 contenant la valeur -1 pour indiquer une erreur
+            return new double[1, 1] { { -1 } };
         }
-
         else
         {
-            double[,] result = new double [matrix1.GetLength(0), matrix1.GetLength(1)];
-            for( int i = 0; i < matrix1.GetLength(0); i++)
-            {
+            // Création d'une nouvelle matrice pour stocker le résultat de l'addition
+            double[,] result = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
 
-                for (int j = 0 ; j < matrix1.GetLength(1); j++)
+            // Boucles imbriquées pour parcourir les éléments des matrices et effectuer l'addition
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.GetLength(1); j++)
                 {
-                    result[i,j] = matrix1[i,j] + matrix2[i,j];
+                    // Addition des éléments correspondants de matrix1 et matrix2
+                    result[i, j] = matrix1[i, j] + matrix2[i, j];
                 }
             }
+
+            // Retourne la matrice résultante après l'addition
             return result;
         }
-
     }
 }
