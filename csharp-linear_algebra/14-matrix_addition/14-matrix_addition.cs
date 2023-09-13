454a3bffe3b2e31286f4edd9141041061304db0a
Rbrogniez <5013@holbertonschool.com>
2023-09-13 16:10:07 +0200

    14. Matrix addition #1

---
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        if (matrix1 && matrix2 < 2 || matrix1 && matrix2 > 3 || matrix1 != matrix2)
+        {
+            return new double[,] {{-1}};
+        }
+
+        else
+        {
+            double[,] result = new double (matrix1.GetLength(0), matrix1.GetLength(1));
+            for( i = 0; matrix1.GetLength(0); i++)
+            {
+
+                for (j = 0 ; matrix1.GetLength(1); j++)
+                {
+                    result matrix1[i,j] + matrix2[i,j];
+                }
+            }
+            return result;
+        }
+    }
+}
