094a18b76617c721924e22a71d516bf7ddb8fe17
Rbrogniez <5013@holbertonschool.com>
2023-09-13 11:58:30 +0200

    comment the code

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -2,27 +2,35 @@
 
 public class VectorMath
 {
+    // Méthode pour additionner deux vecteurs
     public static double[] Add(double[] vector1, double[] vector2)
     {
-        if(vector1.Length < 2 || vector1.Length > 3 && vector2.Length < 2 || vector2.Length > 3)
+        // Vérifier si la longueur des vecteurs est inférieure à 2 ou supérieure à 3
+        if (vector1.Length < 2 || vector1.Length > 3 && vector2.Length < 2 || vector2.Length > 3)
         {
+            // Si les conditions ci-dessus sont remplies, retourner un tableau contenant -1
             return new double[] { -1 };
         }
-        if(vector1.Length != vector2.Length)
+
+        // Vérifier si les longueurs des vecteurs ne correspondent pas
+        if (vector1.Length != vector2.Length)
         {
+            // Si les longueurs ne correspondent pas, retourner un tableau contenant -1
             return new double[] { -1 };
         }
         else
         {
+            // Si les longueurs des vecteurs sont valides et correspondent, effectuer l'addition
             double[] result = new double[vector1.Length];
-            for(int i = 0 ; i < vector1.Length ; i++)
+            for (int i = 0; i < vector1.Length; i++)
             {
-              result[i] =  vector1[i] + vector2[i];
+                result[i] = vector1[i] + vector2[i];
             }
+            // Retourner le résultat de l'addition
             return result;
         }
-
     }
+}
 
    // public static void Main()
    // {
