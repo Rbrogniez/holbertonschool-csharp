279adf5e57b51413b7fcf12a476986fe96f7d4c3
Rbrogniez <105486378+Rbrogniez@users.noreply.github.com>
2023-09-17 17:48:15 +0200

    20-matrix_rotate_2D

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Windows.Media;
 
 /// <summary>
-/// New matrix class calculation
+/// class MatrixMath
 /// </summary>
 class MatrixMath
 {
+    /// <summary>
+    /// Rotates a square 2D matrix by a given angle in radians
+    /// </summary>
+    /// <param name="matrix"> The matrix </param>
+    /// <param name="angle">The angle in radians </param>
+    /// <returns> The resulting matrix or -1 </returns>
     public static double[,] Rotate2D(double[,] matrix, double angle)
     {
-        double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
-        matrix.Rotate(angle);
-        return matrix;
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return new double[,] { { -1 } };
+
+        double cosTheta = Math.Cos(angle);
+        double sinTheta = Math.Sin(angle);
+
+        double[,] rotatedMatrix = new double[2, 2];
+
+        rotatedMatrix[0, 0] = Math.Round(matrix[0, 0] * cosTheta + matrix[0, 1] * -sinTheta, 2);
+        rotatedMatrix[0, 1] = Math.Round(matrix[0, 0] * sinTheta + matrix[0, 1] * cosTheta, 2);
+        rotatedMatrix[1, 0] = Math.Round(matrix[1, 0] * cosTheta + matrix[1, 1] * -sinTheta, 2);
+        rotatedMatrix[1, 1] = Math.Round(matrix[1, 0] * sinTheta + matrix[1, 1] * cosTheta, 2);
+
+        return rotatedMatrix;
     }
 }
+
