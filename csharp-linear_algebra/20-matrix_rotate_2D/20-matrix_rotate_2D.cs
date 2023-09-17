6378169e65a409cdd6fbbea108cf00896b052c6c
Rbrogniez <105486378+Rbrogniez@users.noreply.github.com>
2023-09-17 17:35:45 +0200

    20-matrix_rotate_2D

---
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Media;
+
+/// <summary>
+/// New matrix class calculation
+/// </summary>
+class MatrixMath
+{
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        matrix.Rotate(angle);
+        return matrix;
+    }
+}
