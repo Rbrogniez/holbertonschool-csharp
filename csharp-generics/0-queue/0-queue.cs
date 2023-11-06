11d6b849bea6c6f1102081238c91a3ee0561db5c
Rbrogniez <5013@holbertonschool.com>
2023-11-06 13:43:10 +0100

    back to old version

--- csharp-generics/0-queue/0-queue.cs
+++ csharp-generics/0-queue/0-queue.cs
@@ -1,10 +1,8 @@
-﻿public class Queue<T>
+﻿using System;
+
+public class Queue<T>
 {
-    /// <summary>
-    /// Returns the type of elements stored in the queue.
-    /// </summary>
-    /// <returns>The <see cref="System.Type"/> of the elements in the queue.</returns>
-    public Type CheckType()
+      public Type CheckType()
     {
         return typeof(T);
     }
