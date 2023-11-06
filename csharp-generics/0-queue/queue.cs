1822fc93f2866f8b65c0b20ddb2731a063a359c4
Rbrogniez <5013@holbertonschool.com>
2023-11-06 13:23:55 +0100

    adding queue.cs

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -2,7 +2,23 @@
 
 public class Queue<T>
 {
-      public Type CheckType()
+    // Private class to represent nodes in the singly linked list
+    private class Node
+    {
+        public T Value { get; set; }
+        public Node Next { get; set; }
+    }
+
+    private Node head;  // Head of the linked list
+
+    // Constructor to initialize the queue
+    public Queue()
+    {
+        head = null;
+    }
+
+    // Method to check and return the type of the queue
+    public Type CheckType()
     {
         return typeof(T);
     }
