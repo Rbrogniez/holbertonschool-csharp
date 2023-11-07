7622dccf30bdd3237c86bf28d7cd5b7855223467
Rbrogniez <5013@holbertonschool.com>
2023-11-07 12:07:32 +0100

    Enqueue

---
+++ csharp-generics/1-enqueue/1-enqueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Queue<T>
+{
+    public class Node
+    {
+        public T value;
+        public Node next;
+
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int count;
+
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        count++;
+    }
+
+    public int Count()
+    {
+        return count;
+    }
+}
