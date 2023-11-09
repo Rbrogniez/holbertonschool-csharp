fc7e5b34a74c33164c0a75956c8f911fbde75a2e
Rbrogniez <5013@holbertonschool.com>
2023-11-09 16:11:38 +0100

    print

---
+++ csharp-generics/4-print/4-print.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+public class Queue<T>
+{
+    public class Node
+    {
+        public T value;
+        public Node next;
+
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int count;
+
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        count++;
+    }
+
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+
+        T dequeuedValue = head.value;
+        head = head.next;
+        count--;
+
+        return dequeuedValue;
+    }
+
+    public T Peek()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+
+        return head.value;
+    }
+
+    public void Print()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return;
+        }
+
+        Node current = head;
+        while (current != null)
+        {
+            Console.Write($"{current.value} ");
+            current = current.next;
+        }
+        Console.WriteLine();
+    }
+
+    public int Count()
+    {
+        return count;
+    }
+}
