704eb9e9a84e3ec0d3049244278e23c63c62e873
Rbrogniez <5013@holbertonschool.com>
2023-11-09 12:05:05 +0100

    dequeue

---
+++ csharp-generics/2-dequeue/2-dequeue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class Queue<T>
+{
+    public class Node
+    {
+        public T value;
+        public Node next;
+
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int count;
+
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        count++;
+    }
+
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+
+        T dequeuedValue = head.value;
+        head = head.next;
+        count--;
+
+        return dequeuedValue;
+    }
+
+    public int Count()
+    {
+        return count;
+    }
+}
