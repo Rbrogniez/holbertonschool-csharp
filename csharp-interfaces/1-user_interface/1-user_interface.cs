e98ff5da7ce24ed1a82a9864073eb2d76bac76ac
Rbrogniez <5013@holbertonschool.com>
2023-11-12 18:46:50 +0100

    user interface

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,55 +1,40 @@
 ﻿using System;
 
-// Define the IInteractive interface
+public abstract class Base
+{
+    string name { get; }
+
+    public override string ToString() => $"{name} is a {GetType()}";
+}
+
 public interface IInteractive
 {
     void Interact();
 }
 
-// Define the IBreakable interface
 public interface IBreakable
 {
-    int Durability { get; set; }
+    int durability { get; set; }
     void Break();
 }
 
-// Define the ICollectable interface
 public interface ICollectable
 {
-    bool IsCollected { get; set; }
+    bool isCollected { get; set; }
     void Collect();
 }
 
-// Define the Base class
-public abstract class Base
-{
-    public string name;
-
-    public override string ToString() => $"{name} is a {GetType()}";
-}
-
-// Define the TestObject class that inherits from Base and implements all three interfaces
 public class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
-    // Properties with traditional get and set methods
-    public int Durability { get; set; }
-    public bool IsCollected { get; set; }
+    public void Interact(){}
 
-    // Explicitly implement IInteractive interface
-    public void Interact()
-    {
-        // Implementation goes here
-    }
+    public int durability { get; set; }
 
-    // Explicitly implement IBreakable interface
-    public void Break()
-    {
-        // Implementation goes here
-    }
+    public void Break(){}
 
-    // Explicitly implement ICollectable interface
-    public void Collect()
-    {
-        // Implementation goes here
-    }
+    public bool isCollected { get; set; }
+
+    public void Collect(){}
+
+    public string name { get; set; }
 }
