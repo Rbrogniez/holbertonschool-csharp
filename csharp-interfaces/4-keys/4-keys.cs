57ded12d2afd56c04b7cb2df7b8f041f597a96fb
Rbrogniez <5013@holbertonschool.com>
2023-11-12 23:27:52 +0100

    keys

---
+++ csharp-interfaces/4-keys/4-keys.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+public class RoomObjects
+{
+    public static void IterateAction(List<Base> roomObjects, Type type)
+    {
+        foreach (var obj in roomObjects)
+        {
+            // Check if the object implements the specified interface type
+            if (type.IsAssignableFrom(obj.GetType()))
+            {
+                // Use reflection to find and invoke the method
+                MethodInfo methodInfo = type.GetMethod("Collect");
+                if (methodInfo != null)
+                {
+                    // Invoke the method
+                    methodInfo.Invoke(obj, null);
+                }
+            }
+        }
+    }
+}
