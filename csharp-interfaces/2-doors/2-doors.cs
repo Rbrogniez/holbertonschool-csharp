a81d82053c6cd7df21db3ace1473d161092dc6b6
Rbrogniez <5013@holbertonschool.com>
2023-11-12 19:11:52 +0100

    2-doors

---
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public abstract class Base
+{
+    public string name { get; }
+
+    protected Base(string name = "Door")
+    {
+        this.name = name;
+    }
+
+    public override string ToString() => $"{name} is a {GetType()}";
+}
+
+public interface IInteractive
+{
+    void Interact();
+}
+
+public class Door : Base, IInteractive
+{
+    public Door(string name = "Door") : base(name)
+    {
+    }
+
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
+
