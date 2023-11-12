da7da6945ff5e1e8036f52bd5c6c7fb187be6e80
Rbrogniez <5013@holbertonschool.com>
2023-11-12 17:53:03 +0100

    Abstract thinking

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,12 @@
+﻿﻿using System;
+
+/// <summary> Represents an abstract base class </summary>
+public abstract class Base
+{
+    /// <summary> Gets or sets the name associated with the Base class. </summary>
+    public string name;
+
+    /// <summary> Overrides the default ToString method </summary>
+    /// <returns> String </returns>
+    public override string ToString() => $"{name} is a {GetType()}";
+}
